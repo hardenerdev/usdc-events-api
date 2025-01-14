14f24ce83c155a5fe38df33d919f821d1d37ed2e
hardenerdev <hardenerdev@gmail.com>
2025-01-14 08:01:00 +0100

    refactor: filter validation schema into schemas folder

--- src/middleware/transferevent.js
+++ src/middleware/transferevent.js
@@ -1,43 +1,5 @@
 const { checkSchema, validationResult } = require('express-validator');
-
-const filterValidationSchema = {
-  from: {
-    trim: true,
-    optional: true,
-    isEthereumAddress: true,
-    errorMessage: 'from is not an valid ethereum address',
-  },
-  to: {
-    trim: true,
-    isEthereumAddress: true,
-    optional: true,
-    errorMessage: 'to is not an valid ethereum address',
-  },
-  initDate: {
-    trim: true,
-    isISO8601: true,
-    optional: true,
-    errorMessage: 'initDate is not an ISO8601 compatible date',
-  },
-  endDate: {
-    trim: true,
-    isISO8601: true,
-    optional: true,
-    errorMessage: 'endDate is not an ISO8601 compatible date',
-  },
-  minTokens: {
-    trim: true,
-    isNumeric: true,
-    optional: true,
-    errorMessage: 'minTokens is not numeric',
-  },
-  maxTokens: {
-    trim: true,
-    isNumeric: true,
-    optional: true,
-    errorMessage: 'minTokens is not numeric',
-  },
-};
+const filterValidationSchema = require('../schemas/filtervalidation');
 
 const checkFilters = async (req, res, next) => {
   console.log(`? checking filters from middleware`);
